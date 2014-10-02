4715d7b2512b94574cfc9b7b78279d1e4e0b23aa
jyy <njujiangyy@gmail.com>
2014-10-02 23:54:22 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  23:54:22 up  7:58,  3 users,  load average: 0.00, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---vi      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -223,7 +223,7 @@ uint32_t eval(int p,int q)
 		}
 		val1=eval(p,op-1);
 		val2=eval(op+1,q);
-		printf("%d %d\n",val1,val2);
+		printf("-%d %d\n",val1,val2);
  		switch(tokens[op].type){
 		 	case '+':return val1+val2;
 			case '-':return val1-val2;
