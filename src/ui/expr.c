d9ac3d89df729586c6e7c1e4424a0e6decd2a04b
jyy <njujiangyy@gmail.com>
2014-10-03 01:09:42 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  01:09:42 up  9:14,  4 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/ui/expr.c
+++ src/ui/expr.c
@@ -222,7 +222,7 @@ uint32_t eval(int p,int q)
 	//		printf("%d\n",num);
 			return num;
 		}
-		printf("%d\n",op);
+//		printf("%d\n",op);
 		val1=eval(p,op-1);
 		val2=eval(op+1,q);
 	//	printf("-%d %d\n",val1,val2);
