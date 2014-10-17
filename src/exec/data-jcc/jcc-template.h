8ef01048cf72244f35b1a886a9d2afe38651a701
jyy <njujiangyy@gmail.com>
2014-10-17 19:57:43 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:57:43 up  4:50, 13 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-8*[sshd---sshd---bash---vi]      |      |-3*[sshd---sshd---bash]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/data-jcc/jcc-template.h
+++ src/exec/data-jcc/jcc-template.h
@@ -7,8 +7,8 @@ make_helper(concat(je_rel_, SUFFIX)) {
 		DATA_TYPE rel = instr_fetch(eip + 1, DATA_BYTE);
 	    printf("%x\n",rel);	
 		print_asm("je" str(SUFFIX) " %x", eip + rel + 2); 
-		if (cpu.ZF == 1)return DATA_BYTE + 2;
-		else return rel + 2;
+		if (cpu.ZF == 1)return DATA_BYTE + 1;
+		else return rel + 1;
 }
 
 #include "exec/template-end.h"
