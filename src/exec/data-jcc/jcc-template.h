19f58ec5d6ab46485987c9f714d55a163293c71e
jyy <njujiangyy@gmail.com>
2014-10-17 19:37:15 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:37:15 up  4:30, 13 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-8*[sshd---sshd---bash---vi]      |      |-3*[sshd---sshd---bash]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/data-jcc/jcc-template.h
+++ src/exec/data-jcc/jcc-template.h
@@ -4,7 +4,7 @@
 #include "cpu/reg.h"
 
 make_helper(concat(je_rel_, SUFFIX)) {
-		DATA_TYPE rel = instr_fetch(eip, DATA_BYTE);
+		DATA_TYPE rel = instr_fetch(eip + 1, DATA_BYTE);
 	    printf("%x\n",rel);	
 		print_asm("je" str(SUFFIX) " %x", eip + rel + 1); 
 		if (cpu.ZF == 1)return DATA_BYTE + 1;
