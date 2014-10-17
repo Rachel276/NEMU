dcfa2569e0c9b44520ad01022db3f5a34df41cad
jyy <njujiangyy@gmail.com>
2014-10-17 19:28:12 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:28:12 up  4:21, 12 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-8*[sshd---sshd---bash---vi]      |      |-2*[sshd---sshd---bash]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/data-test/test-template.h
+++ src/exec/data-test/test-template.h
@@ -6,7 +6,7 @@
 make_helper(concat(test_i2r_, SUFFIX)){
     DATA_TYPE imm = instr_fetch(eip ,DATA_BYTE);
 	int reg_code = instr_fetch(eip + DATA_BYTE, 1) & 0x7;
-	REG(reg_code) = REG(reg_code) & imm;
+//	REG(reg_code) = REG(reg_code) & imm;
 	cpu.CF = cpu.OF = 0;
 
 	print_asm("test" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(reg_code));
@@ -18,38 +18,38 @@ make_helper(concat(test_i2rm_, SUFFIX)){
 	DATA_TYPE imm = instr_fetch(eip ,DATA_BYTE);
 	m.val = instr_fetch(eip + DATA_BYTE + 1, 1);
 	if (m.mod == 3){
-		REG(m.R_M) = REG(m.R_M) & imm;
+//		REG(m.R_M) = REG(m.R_M) & imm;
 		cpu.CF = cpu.OF = 0;
 		print_asm("test" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 1 + DATA_BYTE + 1;
-	}
+ 	}
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + DATA_BYTE + 1, &addr);
-		MEM_W(addr, MEM_R(addr) & imm);
+//		MEM_W(addr, MEM_R(addr) & imm);
 		cpu.CF = cpu.OF = 0;
 		print_asm("test" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + DATA_BYTE + 1;
-	}
+ 	}
 }
 
 make_helper(concat(test_r2rm_, SUFFIX)){
 	ModR_M m;
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3){
-		REG(m.reg) = REG(m.reg) & REG(m.R_M);
+//		REG(m.reg) = REG(m.reg) & REG(m.R_M);
 		cpu.CF = cpu.OF = 0;
 		print_asm("test" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.reg), REG_NAME(m.R_M));
 		return 2;
-	}
+ 	}
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
-		REG(m.reg) = MEM_R(addr) & REG(m.reg);
+//		REG(m.reg) = MEM_R(addr) & REG(m.reg);
 
 		print_asm("test" str(SUFFIX) " %%%s,%s",REG_NAME(m.reg), ModR_M_asm);
 		return len + 1;
-	}
+ 	}
 }
 
 #include "exec/template-end.h"
