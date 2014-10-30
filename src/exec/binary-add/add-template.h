9f9ea1283d1707d202d3cbc16b635701db77fe42
jyy <njujiangyy@gmail.com>
2014-10-30 21:59:06 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  21:59:06 up  4:43,  8 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-6*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/binary-add/add-template.h
+++ src/exec/binary-add/add-template.h
@@ -6,20 +6,19 @@ make_helper(concat(add_i2a_, SUFFIX)) {
 	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
 	DATA_TYPE res = REG(R_EAX) + imm;
 	REG(R_EAX) = res;
-
+	
 	DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-	if ((REG(R_EAX) && sf) != (imm && sf))cpu.OF = 1;
+	if (MSB(REG(R_EAX)) == MSB(imm) && MSB(res) != MSB(imm))cpu.OF = 1;
 	else cpu.OF = 0;
-	if (res && sf)cpu.SF = 1;
-	else cpu.SF = 0;
+	cpu.CF = MSB(res);
 	if (res == 0)cpu.ZF = 1;
 	else cpu.ZF = 0;
-	if (REG(R_EAX) < imm)cpu.CF = 1;
+	if (res < REG(R_EAX) && res < imm)cpu.CF = 1;
 	else cpu.CF = 0;
 	int i,num=0;
-	for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-	{
-		if (sf && res == 1)num++;
+	for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+	{ 
+		if ((sf & res) == sf)num++;
 		sf <<= 1;
 	}
 	if (num % 2 == 1)cpu.PF = 1;
@@ -39,18 +38,17 @@ make_helper(concat(add_i2rm_, SUFFIX)) {
 		REG(m.R_M) = res;
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((REG(m.R_M) && sf) != (imm && sf))cpu.OF = 1;
+		if (MSB(REG(m.R_M)) == MSB(imm) && MSB(res) != MSB(imm))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (REG(m.R_M) < imm)cpu.CF = 1;
+		if (res < REG(m.R_M) && res < imm)cpu.CF = 1;
 		else cpu.CF = 0;
 		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
@@ -58,7 +56,7 @@ make_helper(concat(add_i2rm_, SUFFIX)) {
 
 		print_asm("add" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 1 + DATA_BYTE + 1;
-	}
+	} 
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
@@ -67,18 +65,17 @@ make_helper(concat(add_i2rm_, SUFFIX)) {
 		MEM_W(addr,res);
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((MEM_R(addr) && sf) != (imm && sf))cpu.OF = 1;
+		if (MSB(MEM_R(addr)) == MSB(imm) && MSB(res) != MSB(imm))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (MEM_R(addr) < imm)cpu.CF = 1;
+		if (res < MEM_R(addr) && res < imm)cpu.CF = 1;
 		else cpu.CF = 0;
 		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
@@ -86,31 +83,33 @@ make_helper(concat(add_i2rm_, SUFFIX)) {
 
 		print_asm("add" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + DATA_BYTE + 1;
-	}
+	} 
 }
 
 make_helper(concat(add_ib2rm_, SUFFIX)){
 	ModR_M m;
-	uint8_t imm;
+	DATA_TYPE imm; 
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3){
-		imm = instr_fetch(eip + 1 + 1, 1);
+		int i,num=0;
+		imm = instr_fetch(eip + 1 + 1, 1) & 0xff;
+		DATA_TYPE sf = imm >> 7;
+		for (i = 8, sf = sf << 8 ; i <= DATA_BYTE * 8; i ++, sf <<= 1) 
+		   imm = imm | sf;	
+
 		DATA_TYPE res = REG(m.R_M) + imm;
 		REG(m.R_M) = res;
 
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((REG(m.R_M) && sf) != (imm && (1 << 7)))cpu.OF = 1;
+		if (MSB(REG(m.R_M)) == MSB(imm) && MSB(res) != MSB(imm))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (REG(m.R_M) < imm)cpu.CF = 1;
+		if (res < REG(m.R_M) && res < imm)cpu.CF = 1;
 		else cpu.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
@@ -119,26 +118,28 @@ make_helper(concat(add_ib2rm_, SUFFIX)){
 		print_asm("add" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 3;
 	}
-	else {
+	else { 
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
-		imm = instr_fetch(eip + 1 + len, 1);
+		int i,num=0;
+		imm = instr_fetch(eip + 1 + len, 1) & 0xff;
+		DATA_TYPE sf = imm >> 7;
+		for (i = 8, sf = sf << 8 ; i <= DATA_BYTE * 8; i ++, sf <<= 1)
+			imm = imm | sf;
+
 		DATA_TYPE res = MEM_R(addr) + imm;
 		MEM_W(addr,res);
 
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((MEM_R(addr) && sf) != (imm && (1 << 7)))cpu.OF = 1;
+		if (MSB(MEM_R(addr)) == MSB(imm) && MSB(res) != MSB(imm))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (MEM_R(addr) < imm)cpu.CF = 1;
+		if (res < MEM_R(addr) && res < imm)cpu.CF = 1;
 		else cpu.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
@@ -157,18 +158,17 @@ make_helper(concat(add_r2rm_, SUFFIX)) {
 		REG(m.R_M) = res;
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((REG(m.R_M) && sf) != (REG(m.reg) && sf))cpu.OF = 1;
+		if (MSB(REG(m.R_M)) == MSB(REG(m.reg)) && MSB(res) != MSB(REG(m.reg)))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (REG(m.R_M) < REG(m.reg))cpu.CF = 1;
+		if (res < REG(m.R_M) && res < REG(m.reg))cpu.CF = 1;
 		else cpu.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		int i,num = 0;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
@@ -176,7 +176,7 @@ make_helper(concat(add_r2rm_, SUFFIX)) {
 
 		print_asm("add" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.reg), REG_NAME        (m.R_M));
 		return 2;
-	}
+	} 
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
@@ -184,26 +184,25 @@ make_helper(concat(add_r2rm_, SUFFIX)) {
 		MEM_W(addr,res);
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((MEM_R(addr) && sf) != (REG(m.reg) && sf))cpu.OF = 1;
+		if (MSB(MEM_R(addr)) == MSB(REG(m.reg)) && MSB(res) != MSB(REG(m.reg)))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
 		if (MEM_R(addr) < REG(m.reg))cpu.CF = 1;
 		else cpu.CF = 0;
 		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
 		{
-			if (sf && res == 1)num++;
+			if (sf && res == sf)num++;
 			sf <<= 1;
-		}
+		} 
 		if (num % 2 == 1)cpu.PF = 1;
 		else cpu.PF = 0;
 
 		print_asm("add" str(SUFFIX) " %%%s,%s",REG_NAME(m.reg), ModR_M_asm)        ;
 		return len + 1;
-	}
+	}  
 }
 
 make_helper(concat(add_rm2r_, SUFFIX)) {
@@ -214,25 +213,24 @@ make_helper(concat(add_rm2r_, SUFFIX)) {
 		REG(m.reg) = res;
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((REG(m.reg) && sf) != (REG(m.R_M) && sf))cpu.OF = 1;
+		if (MSB(REG(m.reg)) == MSB(REG(m.R_M)) && MSB(res) != MSB(m.R_M))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+		cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (REG(m.reg) < REG(m.R_M))cpu.CF = 1;
+		if (res < REG(m.reg) && res < REG(m.R_M))cpu.CF = 1;
 		else cpu.CF = 0;
 		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
 		else cpu.PF = 0;
-		print_asm("add" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.R_M), REG_NAME(        m.reg));
+		print_asm("add" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.R_M), REG_NAME(m.reg));
 		return 2;
-	}
+	} 
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
@@ -240,25 +238,24 @@ make_helper(concat(add_rm2r_, SUFFIX)) {
 		REG(m.reg) = res;
 
 		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if ((MEM_R(addr) && sf) != (REG(m.reg) && sf))cpu.OF = 1;
+		if (MSB(MEM_R(addr)) == MSB(REG(m.reg)) && MSB(res) != MSB(MEM_R(addr)))cpu.OF = 1;
 		else cpu.OF = 0;
-		if (res && sf)cpu.SF = 1;
-		else cpu.SF = 0;
+	    cpu.SF = MSB(res);
 		if (res == 0)cpu.ZF = 1;
 		else cpu.ZF = 0;
-		if (REG(m.reg) < MEM_R(addr))cpu.CF = 1;
+		if (res < REG(m.reg) && res < MEM_R(addr))cpu.CF = 1;
 		else cpu.CF = 0;
 		int i,num=0;
-		for (i = 0,sf = 1,res = res & 0xff;i < 8; i++)
-		{
-			if (sf && res == 1)num++;
+		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
+		{ 
+			if (sf && res == sf)num++;
 			sf <<= 1;
 		}
 		if (num % 2 == 1)cpu.PF = 1;
 		else cpu.PF = 0;
 		print_asm("add" str(SUFFIX) " %%%s,%s", REG_NAME(m.reg), ModR_M_asm)        ;
 		return len + 1;
-	}
+	} 
 }
 
 #include "exec/template-end.h"
