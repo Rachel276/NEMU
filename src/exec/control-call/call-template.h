85143c4ddfb27c8a301011e8fe5305d657bc421d
jyy <njujiangyy@gmail.com>
2014-11-09 11:27:51 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:27:51 up  2:19,  6 users,  load average: 0.02, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-4*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/control-call/call-template.h
+++ src/exec/control-call/call-template.h
@@ -8,12 +8,13 @@ make_helper(concat(call_rel_, SUFFIX)) {
 	DATA_TYPE_S rel = instr_fetch(eip + 1, DATA_BYTE);
 	REG(R_ESP) = REG(R_ESP) - DATA_BYTE;
 	MEM_W(REG(R_ESP),eip);
-	if (DATA_BYTE == 2)
-		cpu.eip = (cpu.eip + rel + DATA_BYTE + 1) & 0x0000ffff;
-	else cpu.eip = cpu.eip + rel + DATA_BYTE + 1;
 	printf("%x %x %x\n",eip,cpu.eip,rel);
 	print_asm("call" str(SUFFIX) " %x",eip + rel + DATA_BYTE + 1);
-	return DATA_BYTE + 1;
+	if (DATA_BYTE == 2){
+		eip = (eip + rel + DATA_BYTE + 1) & 0x0000ffff;
+		return 0;
+	}
+	else return rel + 1 + DATA_BYTE;
 }
 
 make_helper(concat(call_rm_, SUFFIX)) {
@@ -23,11 +24,12 @@ make_helper(concat(call_rm_, SUFFIX)) {
 		DATA_TYPE_S res = REG(m.R_M);
 		REG(R_ESP) = REG(R_ESP) - DATA_BYTE;
 	    MEM_W(REG(R_ESP),cpu.eip);	
-		if (DATA_BYTE == 2)
-			cpu.eip = res & 0x0000ffff;
-		else cpu.eip = res;
 		print_asm("call" str(SUFFIX) " *%%%s",REG_NAME(m.R_M));
-		return res + 2;
+		if (DATA_BYTE == 2){
+			eip = (eip + res + 2) & 0x0000ffff;
+			return 0;
+		}
+		else return res + 2;
 	} 
 	else {
 		swaddr_t addr;
@@ -35,11 +37,12 @@ make_helper(concat(call_rm_, SUFFIX)) {
 		DATA_TYPE_S res = MEM_R(addr);
 		REG(R_ESP) = REG(R_ESP) - DATA_BYTE;
 		MEM_W(REG(R_ESP),cpu.eip);
-		if (DATA_BYTE == 2)
-			cpu.eip = res & 0x0000ffff;
-		else cpu.eip = res;
 		print_asm("call" str(SUFFIX) " *%s",ModR_M_asm);
-		return res + len + 1;
+		if (DATA_BYTE == 2){
+			eip = (eip + res + len + 1) & 0x0000ffff;
+			return 0;
+		}
+		else return res + len + 1;
 	}
 }
 
