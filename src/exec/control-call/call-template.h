a2a6e9e50bb4963e12b77ddac1dd2c01ec4984a0
jyy <njujiangyy@gmail.com>
2014-11-09 11:14:04 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  11:14:04 up  2:05,  6 users,  load average: 0.08, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-4*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/control-call/call-template.h
+++ src/exec/control-call/call-template.h
@@ -11,6 +11,7 @@ make_helper(concat(call_rel_, SUFFIX)) {
 	if (DATA_BYTE == 2)
 		cpu.eip = (cpu.eip + rel) & 0x0000ffff;
 	else cpu.eip = cpu.eip + rel;
+	printf("%x\n",eip);
 	print_asm("call" str(SUFFIX) " %x",eip + rel + 2);
 	return rel + 2;
 }
