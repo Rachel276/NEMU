7af9f10e196bd8ec5ec2a23068f9f7bdb49d1679
jyy <njujiangyy@gmail.com>
2014-11-15 14:54:45 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:54:45 up 30 min,  4 users,  load average: 0.00, 0.01, 0.03 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-2*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/data-leave/leave-template.h
+++ src/exec/data-leave/leave-template.h
@@ -6,8 +6,8 @@
 
 make_helper(concat(leave_, SUFFIX)) {
 	REG(R_ESP) = REG(R_EBP);
-	REG(R_EBP) = MEM_R(REG(R_ESP));
-    REG(R_ESP) = REG(R_ESP) + DATA_BYTE;
+	REG(R_EBP) = MEM_R(cpu.esp);
+    cpu.esp +=  DATA_BYTE;
 	printf("%x %x\n",REG(R_ESP),REG(R_EBP));
 	print_asm("leave\n");
 	return 1;
