77941a52be83942354469544b84af71d82d3c6d6
jyy <njujiangyy@gmail.com>
2014-11-15 20:40:48 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:40:48 up  6:16,  8 users,  load average: 1.00, 1.01, 1.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-sshd---sshd---bash-+-7*[make---sh-+-nemu]      |      |                    |              `-tee]      |      |                    `-vi      |      |-4*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash-+-6*[make---sh-+-nemu]      |                           |              `-tee]      |                           `-make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/Binary.h
+++ src/exec/Binary.h
@@ -10,6 +10,7 @@
 #include "binary-idiv/idiv.h"
 #include "logical-test/test.h"
 #include "logical-xor/xor.h"
+#include "logical-and/and.h"
 
 make_helper(bai_i2rm_b)
 {
@@ -18,10 +19,11 @@ make_helper(bai_i2rm_b)
 	switch (m.opcode) { 
 		case 0: return add_i2rm_b(eip);
 	    case 3: return sbb_i2rm_b(eip);
+		case 4: return and_i2rm_b(eip);
 		case 5: return sub_i2rm_b(eip);
 		case 6: return xor_i2rm_b(eip);
 		case 7: return cmp_i2rm_b(eip);
-	}
+ 	}
 	return 0;
 }
 
@@ -32,10 +34,11 @@ make_helper(bai_i2rm_v)
 	switch (m.opcode) {   
 		case 0: return add_i2rm_v(eip);
 		case 3: return sbb_i2rm_v(eip);
+		case 4: return and_i2rm_v(eip);
 		case 5: return sub_i2rm_v(eip);
 		case 6: return xor_i2rm_v(eip);
 		case 7: return cmp_i2rm_v(eip);
-	} 
+	}  
 	return 0;
 }
 
@@ -46,10 +49,11 @@ make_helper(bai_ib2rm_v)
 	switch (m.opcode) { 
 		case 0: return add_ib2rm_v(eip);
 		case 3: return sbb_ib2rm_v(eip);
+		case 4: return and_ib2rm_v(eip);
 		case 5: return sub_ib2rm_v(eip);
 		case 6: return xor_ib2rm_v(eip);
 		case 7: return cmp_ib2rm_v(eip);
-	 }  
+	 }   
 	return 0;
 }
 
@@ -60,7 +64,7 @@ make_helper(bai_rm_b)
 	switch (m.opcode) {
 		case 0: return test_i2rm_b(eip);
 		case 7: return idiv_rm_b(eip);
-	} 
+	}  
 	return 0;
 }
 
