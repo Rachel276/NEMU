7ffeacb1d33b07173b68aa2ec69d67860c31369d
jyy <njujiangyy@gmail.com>
2014-11-21 00:35:01 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:35:01 up 12:57,  9 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-grep      |      |                    `-vi      |      |-3*[sshd---sshd---bash---vi]      |      |-2*[sshd---sshd---bash---make---sh-+-nemu]      |      |                                   `-tee]      |      |-sshd---sshd---bash-+-6*[make---bash-+-grep]      |      |                    |                `-nemu]      |      |                    |-make---sh-+-nemu      |      |                    |           `-tee      |      |                    `-make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash      `-udevd---2*[udevd]

--- src/exec/data-push/push-template.h
+++ src/exec/data-push/push-template.h
@@ -5,9 +5,8 @@
 
 make_helper(concat(push_r_,SUFFIX)){
 	int reg_code = instr_fetch(eip, 1) & 0x7;
-	int reg_m = 0x4;
-	REG(reg_m) = REG(reg_m) - DATA_BYTE;
-	MEM_W(REG(reg_m),REG(reg_code));
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP),REG(reg_code));
 	
 	print_asm("push" str(SUFFIX) " %%%s", REG_NAME(reg_code));
 	return 1;
@@ -15,20 +14,18 @@ make_helper(concat(push_r_,SUFFIX)){
 
 make_helper(concat(push_m_,SUFFIX)){
 	swaddr_t addr;
-	int reg_m  = 0x4;
 	int len = read_ModR_M(eip + 1,&addr);
-	REG(reg_m) = REG(reg_m) - DATA_BYTE;
-	MEM_W(REG(reg_m),MEM_R(addr));
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP),MEM_R(addr));
 	
 	print_asm("push" str(SUFFIX) "%s", ModR_M_asm);
 	return len;
 }
 
 make_helper(concat(push_i_,SUFFIX)){
-	DATA_TYPE imm = instr_fetch(eip, DATA_BYTE);
-	int reg_m = 0x4;
-	REG(reg_m) = REG(reg_m) - DATA_BYTE;
-	MEM_W(REG(reg_m),imm);
+	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
+	reg_l(R_ESP) -=  DATA_BYTE;
+	MEM_W(reg_l(R_ESP), imm);
 
 	print_asm("push" str(SUFFIX) "$0x%x", imm);
 	return DATA_BYTE;
