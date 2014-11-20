d27b7fb030bcad0222d567ea43765c4ab55d1b25
jyy <njujiangyy@gmail.com>
2014-11-20 21:11:06 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  21:11:06 up 10:22,  8 users,  load average: 0.86, 0.37, 0.17 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-grep      |      |                    `-vi      |      |-sshd---sshd---bash      |      |-sshd---sshd---bash---make---sh-+-nemu      |      |                                `-tee      |      |-3*[sshd---sshd---bash---vi]      |      `-sshd---sshd---bash-+-3*[make---bash-+-grep]      |                           |                `-nemu]      |                           `-make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -6,13 +6,13 @@ void loader() {
 	Elf32_Ehdr *elf = (void *)0;
 	Elf32_Phdr *ph = (void *)elf->e_phoff; 
 	int i, j;
-	for (i = 0; i < elf->e_phnum; i ++){
+	for (i = 0; i < elf->e_phnum; i ++,ph++){
 			/* Scan the program header table, load each segment into memory */
-			if(ph[i].p_type == PT_LOAD) {
+			if(ph->p_type == PT_LOAD) {
 					for (j = 0;j < ph[i].p_filesz;j ++)
-						*((uint8_t*)ph[i].p_vaddr + j) = *((uint8_t*)elf + ph[i].p_offset + j);	
+						*((uint8_t*)ph->p_vaddr + j) = *((uint8_t*)elf + ph->p_offset + j);	
 					for (j = ph[i].p_filesz; j < ph[i].p_memsz; j ++)
-						*((uint8_t*)ph[i].p_vaddr + j) = 0;
+						*((uint8_t*)ph->p_vaddr + j) = 0;
 //					memset((void *)(ph[i].p_vaddr + ph[i].p_filesz), 0, sizeof(ph[i].p_memsz - ph[i].p_filesz);			
 			}
 	}
