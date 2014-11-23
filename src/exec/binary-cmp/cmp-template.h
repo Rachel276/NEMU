16e7f949406254efe2d0863692fcb60ddbf36a88
jyy <njujiangyy@gmail.com>
2014-11-23 19:32:30 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  19:32:30 up  3:12, 10 users,  load average: 0.02, 0.02, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash-+-grep      |      |                    `-vi      |      |-5*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash]      `-udevd---2*[udevd]

--- src/exec/binary-cmp/cmp-template.h
+++ src/exec/binary-cmp/cmp-template.h
@@ -3,29 +3,26 @@
 #include "cpu/modrm.h"
 #include "cpu/reg.h"
 
+#define mflags(ret, lhs, imm);\
+	eflags.SF = ((DATA_TYPE_S)(rst) < 0);\
+	eflags.ZF = ((rst) == 0);\
+	eflags.CF = lhs < imm;\
+	eflags.OF = (MSB(lhs) != MSB(imm)) && (MSB(rst) == MSB(imm));\
+	eflags.PF = 0;\
+	while (rst){\
+		eflags.PF = !eflags.PF;\
+		rst = rst & (rst - 1);\
+	}
+
 make_helper(concat(cmp_i2a_, SUFFIX)) {
 	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
-	DATA_TYPE res = REG(R_EAX) - imm;
-	DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-	if (MSB(REG(R_EAX)) != MSB(imm) && MSB(res) == MSB(imm))eflags.OF = 1;
-	else eflags.OF = 0;
-	eflags.SF = MSB(res);
-	if (res == 0)eflags.ZF = 1;
-	else eflags.ZF = 0;
-	if (REG(R_EAX) < imm)eflags.CF = 1;
-	else eflags.CF = 0;
-	int i,num=0;
-	for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-	{ 
-		if ((sf & res) == sf)num++;
-		sf <<= 1;
- 	}
-	if (num % 2 == 1)eflags.PF = 1;
-	else eflags.PF = 0;
-
+	DATA_TYPE rst = REG(R_EAX) - imm;
+	DATA_TYPE lhs = REG(R_EAX);
+	mflags(rst,lhs,imm);
+//	REG(R_EAX) = rst;
 	print_asm("cmp" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(R_EAX));
 	return DATA_BYTE + 1;
-} 
+}  
 
 make_helper(concat(cmp_i2rm_, SUFFIX)) { 
 	ModR_M m;
@@ -33,50 +30,21 @@ make_helper(concat(cmp_i2rm_, SUFFIX)) {
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3){ 
 		imm = instr_fetch(eip + 1 + 1, DATA_BYTE);
-		DATA_TYPE res = REG(m.R_M) - imm;
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(REG(m.R_M)) != MSB(imm) && MSB(res) == MSB(imm))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (REG(m.R_M) < imm)eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
-
+		DATA_TYPE rst = REG(m.R_M) - imm;
+		DATA_TYPE lhs = REG(m.R_M);
+		mflags(rst,lhs,imm);
+	//	REG(m.R_M) = rst;
 		print_asm("cmp" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 1 + DATA_BYTE + 1;
-	}
-	else {
+	}  
+	else { 
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
 		imm = instr_fetch(eip + 1 + len, DATA_BYTE);
-		DATA_TYPE res = MEM_R(addr) - imm;
-		
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(MEM_R(addr)) != MSB(imm) && MSB(res) == MSB(imm))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (MEM_R(addr) < imm)eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == 1)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
-
+		DATA_TYPE rst = MEM_R(addr) - imm;
+		DATA_TYPE lhs = MEM_R(addr);
+		mflags(rst,lhs,imm);
+	//	MEM_W(addr,rst);
 		print_asm("cmp" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + DATA_BYTE + 1;
 	} 
@@ -84,59 +52,25 @@ make_helper(concat(cmp_i2rm_, SUFFIX)) {
 
 make_helper(concat(cmp_ib2rm_, SUFFIX)){
 	ModR_M m;
-	DATA_TYPE imm;
+	DATA_TYPE_S imm;
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3){
-		int i,num = 0;
-		imm = instr_fetch(eip + 1 + 1, 1) & 0xff;
-		DATA_TYPE sf = imm >> 7;
-		for (i = 8, sf = sf << 8; i < DATA_BYTE * 8; i ++, sf <<= 1)
-			imm = imm | sf;
-
-		DATA_TYPE res = REG(m.R_M) - imm;
-		if (MSB(REG(m.R_M)) != MSB(imm) && MSB(res) == MSB(imm))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (REG(m.R_M) < imm)eflags.CF = 1;
-		else eflags.CF = 0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
-
+		imm = (DATA_TYPE_S)(instr_fetch(eip + 1 + 1, 1));
+		DATA_TYPE rst = REG(m.R_M) - imm;
+		DATA_TYPE lhs = REG(m.R_M);
+		mflags(rst,lhs,imm);
+	//	REG(m.R_M) = rst;
 		print_asm("cmp" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 3;
-	} 
-	else {  
+	}  
+	else {    
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
-		int i,num = 0;
-		imm = instr_fetch(eip + 1 + len, 1) & 0xff;
-		DATA_TYPE sf = imm >> 7;
-		for (i = 0, sf = sf << 8; i < DATA_BYTE * 8; i ++, sf <<= 1)
-			imm = imm | sf;
-
-		DATA_TYPE res = MEM_R(addr) - imm;
-		if (MSB(MEM_R(addr)) != MSB(imm) && MSB(res) == MSB(imm))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (MEM_R(addr) < imm)eflags.CF = 1;
-		else eflags.CF = 0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
-
+		imm = (DATA_TYPE_S)(instr_fetch(eip + 1 + len, 1));
+		DATA_TYPE rst = MEM_R(addr) - imm;
+		DATA_TYPE lhs = MEM_R(addr);
+	//	MEM_W(addr,rst);
+		mflags(rst,lhs,imm);
 	//	printf("0x%x 0x%x 0x%x\n",res,MEM_R(addr),imm);
 		print_asm("cmp" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + 2;
@@ -146,50 +80,24 @@ make_helper(concat(cmp_ib2rm_, SUFFIX)){
 make_helper(concat(cmp_r2rm_, SUFFIX)) {
 	ModR_M m;
 	m.val = instr_fetch(eip + 1, 1);
+	DATA_TYPE imm = REG(m.reg);
 	if (m.mod == 3){
-		DATA_TYPE res = REG(m.R_M) - REG(m.reg);
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(REG(m.R_M)) != MSB(REG(m.reg)) && MSB(res) == MSB(REG(m.reg)))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (REG(m.R_M) < REG(m.reg))eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
+		DATA_TYPE rst = REG(m.R_M) - imm;
+		DATA_TYPE lhs = REG(m.R_M);
+		mflags(rst,lhs,imm);
+	//	REG(m.R_M) = rst;
 	//	printf("%d %d\n",REG(m.R_M) ,REG(m.reg));
 
 		print_asm("cmp" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.reg), REG_NAME    (m.R_M));
 		return 2;
 	}
-	else {
+	else { 
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
-		DATA_TYPE res = MEM_R(addr) - REG(m.reg);
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(MEM_R(addr)) != MSB(REG(m.reg)) && MSB(res) == MSB(REG(m.reg)))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (MEM_R(addr) < REG(m.reg))eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
-
+		DATA_TYPE rst = MEM_R(addr) - imm;
+		DATA_TYPE lhs = MEM_R(addr);
+		mflags(rst,lhs,imm);
+	//	MEM_W(addr,rst);
 		print_asm("cmp" str(SUFFIX) " %%%s,%s",REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
 	}
@@ -198,47 +106,22 @@ make_helper(concat(cmp_r2rm_, SUFFIX)) {
 make_helper(concat(cmp_rm2r_, SUFFIX)) {
 	ModR_M m;
 	m.val = instr_fetch(eip + 1, 1);
+	DATA_TYPE lhs = REG(m.reg);
 	if(m.mod == 3) {
-		DATA_TYPE res = REG(m.reg) - REG(m.R_M);
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(REG(m.reg)) != MSB(REG(m.R_M)) && MSB(res) == MSB(REG(m.R_M)))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (REG(m.reg) < REG(m.R_M))eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
+		DATA_TYPE imm = REG(m.R_M);
+		DATA_TYPE rst = lhs - imm;
+		mflags(rst,lhs,imm);
+	//	REG(m.reg) = rst;
 		print_asm("cmp" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.R_M), REG_NAME(    m.reg));
 		return 2;
 	} 
 	else {
 		swaddr_t addr;
-		int len = read_ModR_M(eip + 1, &addr);
-		DATA_TYPE res = REG(m.reg) - MEM_R(addr);
-		DATA_TYPE sf = 1 << (8 * DATA_BYTE - 1);
-		if (MSB(MEM_R(addr)) != MSB(REG(m.reg)) && MSB(res) == MSB(MEM_R(addr)))eflags.OF = 1;
-		else eflags.OF = 0;
-		eflags.SF = MSB(res);
-		if (res == 0)eflags.ZF = 1;
-		else eflags.ZF = 0;
-		if (REG(m.reg) < MEM_R(addr))eflags.CF = 1;
-		else eflags.CF = 0;
-		int i,num=0;
-		for (i = 0,sf = 0x1,res = res & 0xff;i < 8; i++)
-		{ 
-			if ((sf & res) == sf)num++;
-			sf <<= 1;
-		}
-		if (num % 2 == 1)eflags.PF = 1;
-		else eflags.PF = 0;
+		int len = read_ModR_M(eip + 1, &addr);	
+		DATA_TYPE imm = MEM_R(addr);
+		DATA_TYPE rst = lhs - imm;
+		mflags(rst,lhs,imm);
+	//	REG(m.reg) = rst;
 		print_asm("cmp" str(SUFFIX) " %%%s,%s", REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
 	}
