17f428d97593a8c65d9b22604e750c718b541ea4
jyy <njujiangyy@gmail.com>
2014-11-23 23:39:05 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  23:39:05 up  6:30, 12 users,  load average: 0.09, 0.05, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---grep      |      |-7*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash]      `-udevd---2*[udevd]

--- src/exec/binary-sbb/sbb-template.h
+++ src/exec/binary-sbb/sbb-template.h
@@ -6,7 +6,7 @@
 #define mflags(rst,lhs,imm);\
 	eflags.SF = ((DATA_TYPE_S)(rst) < 0);\
 	eflags.ZF = (rst == 0);\
-	eflags.CF = lhs < imm;\
+	eflags.CF = (lhs < imm) || (lhs < imm + eflags.CF);\
 	eflags.OF = (MSB(lhs) != MSB(imm)) && (MSB(rst) == MSB(imm));\
 	eflags.PF = 0;\
 	while (rst){\
@@ -16,7 +16,7 @@
 
 make_helper(concat(sbb_i2a_, SUFFIX)) {
 	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
-	DATA_TYPE rst = REG(R_EAX) - imm + eflags.CF;
+	DATA_TYPE rst = REG(R_EAX) - imm - eflags.CF;
 	DATA_TYPE lhs = REG(R_EAX);
 	REG(R_EAX) = rst;
 	mflags(rst,lhs,imm);
@@ -31,20 +31,21 @@ make_helper(concat(sbb_i2rm_, SUFFIX)) {
 	m.val = instr_fetch(eip + 1, 1);
 	if (m.mod == 3){  
 		imm = instr_fetch(eip + 1 + 1, DATA_BYTE);
-		DATA_TYPE rst = REG(m.R_M) - imm + eflags.CF;
+		DATA_TYPE rst = REG(m.R_M) - imm - eflags.CF;
 		DATA_TYPE lhs = REG(m.R_M);
 		REG(m.R_M) = rst;
 		mflags(rst,lhs,imm);
 
 		print_asm("sbb" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 1 + DATA_BYTE + 1;
-	}
+	} 
 	else {
+
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
 		imm = instr_fetch(eip + 1 + len, DATA_BYTE);
 		DATA_TYPE lhs = MEM_R(addr);
-		DATA_TYPE rst = MEM_R(addr) - imm + eflags.CF;
+		DATA_TYPE rst = MEM_R(addr) - imm - eflags.CF;
 		MEM_W(addr,rst);
 		mflags(rst,lhs,imm);
 
@@ -60,7 +61,7 @@ make_helper(concat(sbb_ib2rm_, SUFFIX)){
 	if (m.mod == 3){ 
 		imm = instr_fetch(eip + 1 + 1, 1);
 	//	printf("%d %d\n",REG(m.R_M),  (DATA_TYPE_S)(imm));
-		DATA_TYPE rst = REG(m.R_M) - (DATA_TYPE_S)(imm) + eflags.CF;
+		DATA_TYPE rst = REG(m.R_M) - (DATA_TYPE_S)(imm) - eflags.CF;
 //		printf("%d\n",rst);
 		DATA_TYPE lhs = REG(m.R_M);
 //		printf("%d\n",rst);
@@ -69,19 +70,19 @@ make_helper(concat(sbb_ib2rm_, SUFFIX)){
 //		printf("%d\n",rst);
 		print_asm("sbb" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 3;
-	} 
+	}  
 	else {  
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
 		imm = instr_fetch(eip + 1 + len, 1);
-		DATA_TYPE rst = MEM_R(addr) - (DATA_TYPE_S)(imm) + eflags.CF;
+		DATA_TYPE rst = MEM_R(addr) - (DATA_TYPE_S)(imm) - eflags.CF;
 		DATA_TYPE lhs = MEM_R(addr);
 		MEM_W(addr,rst);
 		mflags(rst,lhs,(DATA_TYPE_S)(imm));
 
 		print_asm("sbb" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + 2;
-	}  
+	}   
 }
 
 make_helper(concat(sbb_r2rm_, SUFFIX)) {
@@ -89,25 +90,25 @@ make_helper(concat(sbb_r2rm_, SUFFIX)) {
 	m.val = instr_fetch(eip + 1, 1);
 	DATA_TYPE imm = REG(m.reg);
 	if (m.mod == 3){ 
-		DATA_TYPE rst = REG(m.R_M) - imm + eflags.CF;
+		DATA_TYPE rst = REG(m.R_M) - imm - eflags.CF;
 		DATA_TYPE lhs = REG(m.R_M);
 		REG(m.R_M) = rst;
 		mflags(rst,lhs,imm);
 
 		print_asm("sbb" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.reg), REG_NAME    (m.R_M));
 		return 2;
-	}
+	} 
 	else { 
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
-		DATA_TYPE rst = MEM_R(addr) - imm + eflags.CF;
+		DATA_TYPE rst = MEM_R(addr) - imm - eflags.CF;
 		DATA_TYPE lhs = MEM_R(addr);
 		MEM_W(addr,rst);
 		mflags(rst,lhs,imm);
 
 		print_asm("sbb" str(SUFFIX) " %%%s,%s",REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
-	}
+	} 
 }
 
 make_helper(concat(sbb_rm2r_, SUFFIX)) {
@@ -116,24 +117,24 @@ make_helper(concat(sbb_rm2r_, SUFFIX)) {
 	DATA_TYPE lhs = REG(m.reg);
 	if(m.mod == 3) {
 		DATA_TYPE imm = REG(m.R_M);
-		DATA_TYPE rst = lhs - imm + eflags.CF;
+		DATA_TYPE rst = lhs - imm - eflags.CF;
 		REG(m.reg) = rst;
 		mflags(rst,lhs,imm);
 		
 		print_asm("sbb" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.R_M), REG_NAME(    m.reg));
 		return 2;
-	}  
+	}   
 	else {
 		swaddr_t addr;
 		int len = read_ModR_M(eip + 1, &addr);
 		DATA_TYPE imm = MEM_R(addr);
-		DATA_TYPE rst = lhs - imm + eflags.CF;
+		DATA_TYPE rst = lhs - imm - eflags.CF;
 		REG(m.reg) = rst;
 		mflags(rst,lhs,imm);
 
 		print_asm("sbb" str(SUFFIX) " %%%s,%s", REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
-	} 
+	}  
 }
 
 #include "exec/template-end.h"
