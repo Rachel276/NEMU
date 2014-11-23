b3291826d67d941eb3f80cb3bbc7dba868d920a6
jyy <njujiangyy@gmail.com>
2014-11-23 20:12:07 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  20:12:07 up  3:51, 10 users,  load average: 0.04, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---grep      |      |-5*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-2*[sshd---sshd---bash]      `-udevd---2*[udevd]

--- src/exec/binary-sub/sub-template.h
+++ src/exec/binary-sub/sub-template.h
@@ -18,8 +18,8 @@ make_helper(concat(sub_i2a_, SUFFIX)) {
 	DATA_TYPE imm = instr_fetch(eip + 1, DATA_BYTE);
 	DATA_TYPE rst = REG(R_EAX) - imm;
 	DATA_TYPE lhs = REG(R_EAX);
-	mflags(rst,lhs,imm);
 	REG(R_EAX) = rst;
+	mflags(rst,lhs,imm);
 
 	print_asm("sub" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(R_EAX));
 	return DATA_BYTE + 1;
@@ -33,8 +33,8 @@ make_helper(concat(sub_i2rm_, SUFFIX)) {
 		imm = instr_fetch(eip + 1 + 1, DATA_BYTE);
 		DATA_TYPE rst = REG(m.R_M) - imm;
 		DATA_TYPE lhs = REG(m.R_M);
-		mflags(rst,lhs,imm);
 		REG(m.R_M) = rst;
+		mflags(rst,lhs,imm);
 
 		print_asm("sub" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 1 + DATA_BYTE + 1;
@@ -45,8 +45,8 @@ make_helper(concat(sub_i2rm_, SUFFIX)) {
 		imm = instr_fetch(eip + 1 + len, DATA_BYTE);
 		DATA_TYPE lhs = MEM_R(addr);
 		DATA_TYPE rst = MEM_R(addr) - imm;
+		MEM_W(addr,rst);
 		mflags(rst,lhs,imm);
-	    MEM_W(addr,rst);
 
 		print_asm("sub" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + DATA_BYTE + 1;
@@ -64,8 +64,8 @@ make_helper(concat(sub_ib2rm_, SUFFIX)){
 		printf("%d\n",rst);
 		DATA_TYPE lhs = REG(m.R_M);
 		printf("%d\n",rst);
-		mflags(rst,lhs,(DATA_TYPE_S)(imm));
 		REG(m.R_M) = rst;
+		mflags(rst,lhs,(DATA_TYPE_S)(imm));
 		printf("%d\n",rst);
 		print_asm("sub" str(SUFFIX) " $0x%x,%%%s", imm, REG_NAME(m.R_M));
 		return 3;
@@ -76,8 +76,8 @@ make_helper(concat(sub_ib2rm_, SUFFIX)){
 		imm = instr_fetch(eip + 1 + len, 1);
 		DATA_TYPE rst = MEM_R(addr) - (DATA_TYPE_S)(imm);
 		DATA_TYPE lhs = MEM_R(addr);
-		mflags(rst,lhs,(DATA_TYPE_S)(imm));
 		MEM_W(addr,rst);
+		mflags(rst,lhs,(DATA_TYPE_S)(imm));
 
 		print_asm("sub" str(SUFFIX) " $0x%x,%s", imm, ModR_M_asm);
 		return len + 2;
@@ -91,8 +91,8 @@ make_helper(concat(sub_r2rm_, SUFFIX)) {
 	if (m.mod == 3){ 
 		DATA_TYPE rst = REG(m.R_M) - imm;
 		DATA_TYPE lhs = REG(m.R_M);
-		mflags(rst,lhs,imm);
 		REG(m.R_M) = rst;
+		mflags(rst,lhs,imm);
 
 		print_asm("sub" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.reg), REG_NAME    (m.R_M));
 		return 2;
@@ -102,8 +102,8 @@ make_helper(concat(sub_r2rm_, SUFFIX)) {
 		int len = read_ModR_M(eip + 1, &addr);
 		DATA_TYPE rst = MEM_R(addr) - imm;
 		DATA_TYPE lhs = MEM_R(addr);
-		mflags(rst,lhs,imm);
 		MEM_W(addr,rst);
+		mflags(rst,lhs,imm);
 
 		print_asm("sub" str(SUFFIX) " %%%s,%s",REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
@@ -117,8 +117,8 @@ make_helper(concat(sub_rm2r_, SUFFIX)) {
 	if(m.mod == 3) {
 		DATA_TYPE imm = REG(m.R_M);
 		DATA_TYPE rst = lhs - imm;
-		mflags(rst,lhs,imm);
 		REG(m.reg) = rst;
+		mflags(rst,lhs,imm);
 		
 		print_asm("sub" str(SUFFIX) " %%%s,%%%s", REG_NAME(m.R_M), REG_NAME(    m.reg));
 		return 2;
@@ -128,8 +128,8 @@ make_helper(concat(sub_rm2r_, SUFFIX)) {
 		int len = read_ModR_M(eip + 1, &addr);
 		DATA_TYPE imm = MEM_R(addr);
 		DATA_TYPE rst = lhs - imm;
-		mflags(rst,lhs,imm);
 		REG(m.reg) = rst;
+		mflags(rst,lhs,imm);
 
 		print_asm("sub" str(SUFFIX) " %%%s,%s", REG_NAME(m.reg), ModR_M_asm)    ;
 		return len + 1;
