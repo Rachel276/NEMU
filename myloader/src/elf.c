01143130d965dcc8300f2c5179fde29d83230d27
jyy <njujiangyy@gmail.com>
2014-11-24 00:21:52 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  00:21:52 up  7:12, 11 users,  load average: 0.22, 0.06, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---grep      |      |-7*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash      `-udevd---2*[udevd]

--- myloader/src/elf.c
+++ myloader/src/elf.c
@@ -4,30 +4,27 @@
 void loader() {
 	/* The ELF file is located at memory address 0 */
 	Elf32_Ehdr *elf = (void*)0;
-
-	uint32_t ph_size = elf->e_phentsize*elf->e_phnum;
-	Elf32_Phdr *ph;
-
-	for (ph = (void*)elf->e_phoff;ph != (void*)(elf->e_phoff+ph_size);ph++) {
+	Elf32_Phdr *ph = (void*)elf->e_phoff;
+	int i, j;
+	for (i = 0;i < elf->e_phnum; i ++,ph ++) {
 		/* Scan the program header table, load each segment into memory */
-		if(ph->p_type == PT_LOAD) {
-			uint8_t *p = (void*)ph->p_vaddr;
-			uint8_t *q = (void*)elf+ph->p_offset;
-			int i;
-			for (i = 0;i < ph->p_filesz;i++)
-				p[i] = q[i];
-			/* TODO: read the content of the segment from the ELF file 
+		if(ph->p_type == PT_LOAD)  { 
+			uint8_t *u = (void*)ph->p_vaddr;
+			uint8_t *v = (void*)elf+ph->p_offset;
+			for (j = 0;j < ph->p_filesz; j ++)
+				u[j] = v[j];
+			/* TODO: rea d the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
 			
-			for (;i < ph->p_memsz;i++)
-				p[i] = 0;
+			for (;j < ph->p_memsz; j ++)
+				u[j] = 0;
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
-			 */
-
+ 			 */ 
+ 
 		}
-	}
+	} 
 
 	/* Here we go! */
 	((void(*)(void)) elf->e_entry) ();
