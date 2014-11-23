77b263568f0e88459e814bc81197996f4e2c2a7c
jyy <njujiangyy@gmail.com>
2014-11-23 18:14:59 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  18:14:59 up  1:54,  8 users,  load average: 0.12, 0.06, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash---grep      |      |-3*[sshd---sshd---bash---vi]      |      |-2*[sshd---sshd---bash]      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/control-ret/ret-template.h
+++ src/exec/control-ret/ret-template.h
@@ -7,7 +7,7 @@ make_helper(concat(ret_, SUFFIX)) {
 	cpu.eip = MEM_R(REG(R_ESP));
 	REG(R_ESP) = REG(R_ESP) + DATA_BYTE;
 	if (DATA_BYTE == 2) cpu.eip &= 0x0000ffff;
-	print_asm("ret\n");
+	print_asm("ret");
 	return 0;
 }
 
