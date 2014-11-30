a9930104e8568e8913046fa791830f8f860020cf
jyy <njujiangyy@gmail.com>
2014-11-30 21:34:00 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  21:34:00 up  3:29,  8 users,  load average: 0.05, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-5*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---make---sh-+-git      |      |                                `-sh---pstree      |      `-sshd---sshd---bash---sudo      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,18 +1,23 @@
 #include "common.h"
+#include "cpu/cache.h"
 
 uint32_t dram_read(hwaddr_t addr, size_t len);
 void dram_write(hwaddr_t addr, size_t len, uint32_t data);
+uint32_t cacheL1_read(hwaddr_t, size_t);
+void cacheL1_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
+//	return dram_read(addr, len);
+	return cacheL1_read(addr, len);
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-	dram_write(addr, len, data);
+//	dram_write(addr, len, data);
+	cacheL1_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
@@ -25,12 +30,14 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 	hwaddr_write(addr, len, data);
 }
 
-static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
+/*static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);
-}
+//	return dram_read(addr, len);
+	return cacheL1_read(addr, len);
+}*/
 
 uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return hwaddr_read_instr(addr, len);
+	//return hwaddr_read_instr(addr, len);
+	return swaddr_read(addr, len);
 }
