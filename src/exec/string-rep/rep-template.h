8de6624017766e4d008ab22cb1823682d30d6cd4
jyy <njujiangyy@gmail.com>
2014-12-17 22:44:17 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  22:44:17 up  7:03,  6 users,  load average: 0.00, 0.01, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd-+-sshd---sshd---bash      |      |-2*[sshd---sshd---bash---vi]      |      |-sshd---sshd---bash---git---pager      |      `-sshd---sshd---bash---make---sh-+-git      |                                       `-sh---pstree      `-udevd---2*[udevd]

--- src/exec/string-rep/rep-template.h
+++ src/exec/string-rep/rep-template.h
@@ -3,13 +3,13 @@
 #include "cpu/modrm.h"
 #include "cpu/reg.h"
 
-int exec(swaddr_t);
+make_helper(exec);
 make_helper(concat(rep_movs_m_, SUFFIX)) {
-	int len = 0;
+	int len = 1;
 	char rep_out[40];
-	while (reg_l(R_ECX)) {
+	while (reg_l(R_CX)) {
 		len = exec(eip + 1);
-		reg_l(R_ECX)--;
+		reg_l(R_CX)--;
 	}
 	strcpy(rep_out,assembly);
 	print_asm("rep" str(SUFFIX) " %s",rep_out);
